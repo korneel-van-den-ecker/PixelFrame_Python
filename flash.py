efc99ead8f585cc54510bad0e08aca80cd9d31cd
korneel-van-den-ecker <korneel_van_den_ecker@hotmail.com>
2020-01-18 23:06:36 +0000

    sinus ok , zoom ok , periode ok

--- flash.py
+++ flash.py
@@ -8,25 +8,43 @@ import math
 #import keyboard
 
 frame = Frame(16,16)
-rood = 0x0000ff
+rood = 0x00f0ff
 hoogte = 16
 breedte = 16
-zoom = 2
+zoom = 1/2
+factor = 2
+speed = 100
+aantalBeelden = math.ceil(speed/factor)
+periode = math.pi *factor
 
 while 1:
     #sinus
-    for x in range(breedte):
+    for y in range(aantalBeelden):
+        for x in range(breedte):
+            #waarde van de hoek van 0 tem 360° 
+            radiaalPositie = ((periode / breedte) * x) + (periode / aantalBeelden * y )
+            #sinus waarde van -1.0 tem 1.0
+            amplitude = math.sin(radiaalPositie)
+            #herschalen naar aantal pixels in de hoogte
+            frameAmplitude = math.ceil((hoogte/zoom) - amplitude*hoogte/zoom)
+            if (frameAmplitude >= 16):
+                frameAmplitude = 15
+            frame.zetKleur(x,frameAmplitude,rood,1)
+        frame.strip.show()
+            #time.sleep(0.05)
+        frame.strip.clear_strip()
+
+    """ for x in range(breedte):
         #waarde van de hoek van 0 tem 360° 
         radiaalPositie = (math.pi * 2 / breedte) * x
         #sinus waarde van -1.0 tem 1.0
-        amplitude = math.sin(radiaalPositie)
+        amplitude = math.tan(radiaalPositie)
         #herschalen naar aantal pixels in de hoogte
         frameAmplitude = math.ceil((hoogte/zoom) - amplitude*hoogte/zoom)
         if (frameAmplitude == 16):
             frameAmplitude = 15
-        frame.zetKleur(x,frameAmplitude,rood,1)
+        frame.zetKleur(frameAmplitude,x,rood,1)
         frame.strip.show()
         time.sleep(0.05)
-    frame.strip.clear_strip()
-    
-        
+    frame.strip.clear_strip() """
+
