4ee541d1d25a3482d0068056adc67775cef77416
korneel-van-den-ecker <korneel_van_den_ecker@hotmail.com>
2020-03-03 22:36:53 +0000

    voor klaar maken animaties voor wepapp

--- flash.py
+++ flash.py
@@ -9,20 +9,20 @@ frame = Frame(16,16)
 
 rood = 0x000000
 blauw = 0x0000ff
-#hoogte = frame.hoogte
+hoogte = frame.hoogte
 breedte = frame.breedte
 teller = 0
 teller1 = 0
 #sinussen = [Sinus(1,2.5,frame)]#,Sinus(2,1/22,frame)]#,Sinus(1,1,frame)]        
-""" 
-sinus = Sinus(1,2,frame)
+ 
+""" sinus = Sinus(1,2,frame)
 sinus.brightnessEffect = False
 sinus.flikkeringEffect = False
-sinus.startAnimatie("sin",1.0,0.0)
- """
+sinus.startAnimatie("sin",1.0,0.0) """
 
-fg = FunctieGenerator(2,2,frame)
 
-fg.VoegFunctieToe(Functie("sin",1/2,0))
-fg.VoegFunctieToe(Functie("sin",1,0))
+fg = FunctieGenerator(1,2,frame)
+fg.flikkeringEffect = True
+fg.VoegFunctieToe(Functie("sin",1/2,1))
+#fg.VoegFunctieToe(Functie("sin",1,0))
 fg.startAnimatie()
