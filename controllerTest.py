2d78eb7e505b46e9fbb96700bd782c9c3a713240
korneel-van-den-ecker <korneel_van_den_ecker@hotmail.com>
2022-11-05 21:31:57 +0000

    besturing met kaske snake ok

--- controllerTest.py
+++ controllerTest.py
@@ -1,38 +1,65 @@
 
 #import evdev
+from multiprocessing import Process
+from threading import Thread
+import time
 from evdev import InputDevice, categorize, ecodes
 #from gameOfSnake import SnakeGame
 from snake import Snake, Move
-from pixelFrame import  Frame
 import sys
+from pixelFrame import  Frame
 #creates object 'gamepad' to store the data
 #you can call it whatever you like
 gamepad = InputDevice('/dev/input/event0')
 
 #prints out device info at start
 print(gamepad)
-snake = Snake(Frame(16,16),3,4,0xff0000)
+snake = Snake(Frame(16,16),4,5,0xff0000)
+volgendeMove = Move.DOWN
 
-while 1 :
-    #evdev takes care of polling the controller in a loop
+def zetParam():
+    global volgendeMove  
+    while 1:
+        snake.move(volgendeMove,False,True)
+        time.sleep(0.5)
 
-    
-    for event in gamepad.read_loop():
-        #filters by event type
-        #print(event.value)
-        #print(event.code)
-        #if event.type == ecodes.EV_KEY:
-            #print(event)
-        #if event.type == ecodes.ABS:
-        if event.code == 17 and event.value == -1:
-            print("OMHOOG")
-            snake.move(Move.UP,True,True)
-        if event.code == 17 and event.value == 1:
-            print("OMLAAG")
-            snake.move(Move.DOWN,True,True)
-        if event.code == 16 and event.value == -1:
-            print("LINKS")
-            snake.move(Move.LEFT,False,True)
-        if event.code == 16 and event.value == 1:
-            print("RECHTS")
-            snake.move(Move.RIGHT,False,True)
+#if __name__ == "__main__":
+#    Process(target = zetParam).start()
+#    #Process(target = laatslangBewegen).start()
+
+t1 = Thread(target=zetParam)
+t1.start()
+
+for event in gamepad.read_loop():
+    print("loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")  
+    if event.code == 17 and event.value == -1:
+        volgendeMove = Move.UP
+        print("OMHOOG")
+        #snake.move(Move.UP,False,True)
+    if event.code == 17 and event.value == 1:
+        volgendeMove = Move.DOWN
+        print("OMLAAG")
+        #snake.move(Move.DOWN,False,True)
+    if event.code == 16 and event.value == -1:
+        volgendeMove = Move.LEFT
+        print("LINKS")
+        #snake.move(Move.LEFT,False,True)
+    if event.code == 16 and event.value == 1:
+        volgendeMove = Move.RIGHT
+        print("RECHTS")
+        #snake.move(Move.RIGHT,False,True)
+    #while 1:
+        #global volgendeMove
+        #reet = volgendeMove
+        #snake.move(reet,False,True)
+        #time.sleep(0.5)
+        #print("NU")
+
+
+#def laatslangBewegen():
+    #while 1:
+        #global volgendeMove
+        #reet = volgendeMove
+        #snake.move(reet,False,True)
+        #time.sleep(0.5)
+
